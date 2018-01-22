5cc13c2ec0defbce96ce3f7212c4fc17da421b07
Longueira, Emiliano <longueiraemiliano@gmail.com>
2018-01-22 17:42:23 -0300

    added form validations

--- TestMakerFreeWebApp/ClientApp/app/components/answer/answer-edit.component.ts
+++ TestMakerFreeWebApp/ClientApp/app/components/answer/answer-edit.component.ts
@@ -1,6 +1,7 @@
 ﻿import { Component, Inject, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
+import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
     selector: "answer-edit",
@@ -11,6 +12,7 @@ import { HttpClient } from "@angular/common/http";
 export class AnswerEditComponent {
     title: string;
     answer: Answer;
+    form: FormGroup;
 
     // this will be TRUE when editing an existing question, 
     // FALSE when creating a new one.
@@ -19,11 +21,12 @@ export class AnswerEditComponent {
     constructor(private activatedRoute: ActivatedRoute,
         private router: Router,
         private http: HttpClient,
-        @Inject('BASE_URL') private baseUrl: string) {
+        @Inject('BASE_URL') private baseUrl: string,
+        private fb: FormBuilder) {
 
         // create an empty object from the Quiz interface
         this.answer = <Answer>{};
-
+        this.createForm();
         var id = +this.activatedRoute.snapshot.params["id"];
 
         // check if we're in edit mode or not
@@ -36,6 +39,7 @@ export class AnswerEditComponent {
             this.http.get<Answer>(url).subscribe(res => {
                 this.answer = res;
                 this.title = "Edit - " + this.answer.Text;
+                this.updateForm();
             }, error => console.error(error));
         }
         else {
@@ -47,7 +51,13 @@ export class AnswerEditComponent {
     onSubmit(question: Question) {
         var url = this.baseUrl + "api/answer";
 
+        var tempAnswer = <Answer>{};
+        tempAnswer.Text = this.form.value.Text;
+        tempAnswer.Value = this.form.value.Value;
+        tempAnswer.QuestionId = this.answer.QuestionId;
+
         if (this.editMode) {
+            tempAnswer.Id = this.answer.Id;
             this.http
                 .post<Answer>(url, question)
                 .subscribe(res => {
@@ -68,5 +78,42 @@ export class AnswerEditComponent {
 
     onBack() {
         this.router.navigate(["question/edit", this.answer.QuestionId]);
+    }
+
+    createForm() {
+        this.form = this.fb.group({
+            Text: ['', Validators.required],
+            Value: [0,
+                [Validators.required,
+                Validators.min(-5),
+                Validators.max(5)]
+            ]
+        });
+    }
+
+    updateForm() {
+        this.form.setValue({
+            Text: this.answer.Text || '',
+            Value: this.answer.Value || 0
+        });
+    }
+
+    getFormControl(name: string) {
+        return this.form.get(name);
+    }
+
+    isValid(name: string) {
+        var e = this.getFormControl(name);
+        return e && e.valid;
+    }
+
+    isChanged(name: string) {
+        var e = this.getFormControl(name);
+        return e && (e.dirty || e.touched);
+    }
+
+    hasError(name: string) {
+        var e = this.getFormControl(name);
+        return this.isChanged(name) && !this.isValid(name);
     }
 }
--- TestMakerFreeWebApp/ClientApp/app/components/result/result-edit.component.ts
+++ TestMakerFreeWebApp/ClientApp/app/components/result/result-edit.component.ts
@@ -1,6 +1,7 @@
 ﻿import { Component, Inject, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
+import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: "result-edit",
@@ -11,6 +12,7 @@ import { HttpClient } from "@angular/common/http";
 export class ResultEditComponent {
     title: string;
     result: Result;
+    form: FormGroup;
 
     // this will be TRUE when editing an existing result, 
     // FALSE when creating a new one.
@@ -19,10 +21,12 @@ export class ResultEditComponent {
     constructor(private activatedRoute: ActivatedRoute,
         private router: Router,
         private http: HttpClient,
-        @Inject('BASE_URL') private baseUrl: string) {
+        @Inject('BASE_URL') private baseUrl: string,
+        private fb: FormBuilder) {
 
         // create an empty object from the Quiz interface
         this.result = <Result>{};
+        this.createForm();
 
         var id = +this.activatedRoute.snapshot.params["id"];
 
@@ -36,6 +40,8 @@ export class ResultEditComponent {
             this.http.get<Result>(url).subscribe(res => {
                 this.result = res;
                 this.title = "Edit - " + this.result.Text;
+                this.updateForm();
+
             }, error => console.error(error));
         }
         else {
@@ -46,8 +52,15 @@ export class ResultEditComponent {
 
     onSubmit(result: Result) {
         var url = this.baseUrl + "api/result";
+        var tempResult = <Result>{};
+        tempResult.MaxValue = this.form.value.MaxValue;
+        tempResult.MinValue = this.form.value.MinValue;
+        tempResult.Text = this.form.value.Text;
+        tempResult.QuizId = this.result.QuizId;
 
         if (this.editMode) {
+            tempResult.Id = this.result.Id;
+
             this.http
                 .post<Result>(url, result)
                 .subscribe(res => {
@@ -68,5 +81,40 @@ export class ResultEditComponent {
 
     onBack() {
         this.router.navigate(["quiz/edit", this.result.QuizId]);
+    }
+
+    createForm() {
+        this.form = this.fb.group({
+            Text: ['', Validators.required],
+            MinValue: ['', Validators.pattern(/^\d*$/)],
+            MaxValue: ['', Validators.pattern(/^\d*$/)]
+        });
+    }
+
+    updateForm() {
+        this.form.setValue({
+            Text: this.result.Text,
+            MinValue: this.result.MinValue || '',
+            MaxValue: this.result.MaxValue || ''
+        });
+    }
+
+    getFormControl(name: string) {
+        return this.form.get(name);
+    }
+
+    isValid(name: string) {
+        var e = this.getFormControl(name);
+        return e && e.valid;
+    }
+
+    isChanged(name: string) {
+        var e = this.getFormControl(name);
+        return e && (e.dirty || e.touched);
+    }
+
+    hasError(name: string) {
+        var e = this.getFormControl(name);
+        return this.isChanged(name) && !this.isValid(name);
     }
 }
